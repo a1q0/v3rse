b3aa07412670de3f873ee24a142f212b5ca0112b
aiqo <1682540+a1q0@users.noreply.github.com>
2022-04-14 07:57:13 +0200

    Move inline functions to VK.h

--- src/RenderEngine/VK/VK.h
+++ src/RenderEngine/VK/VK.h
@@ -31,11 +31,12 @@ struct QueueFamilyIndices {
     }
 };
 
-struct SwapChainSupportDetails {
+struct SurfaceDetails {
     VkSurfaceCapabilitiesKHR vkSurfaceCapabilities;
-    std::vector<VkSurfaceFormatKHR> vkSurfaceFormats;
-    std::vector<VkPresentModeKHR> vkPresentModes;
+    vector<VkSurfaceFormatKHR> vkSurfaceFormats;
+    vector<VkPresentModeKHR> vkPresentModes;
 };
+
 
 namespace VK {
     VkDebugUtilsMessengerCreateInfoEXT vkDefaultDebugUtilsMessengerCreateInfoEXT{};
@@ -43,24 +44,248 @@ namespace VK {
     static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallbackDefault(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData);
     static VKAPI_ATTR VkBool32 VKAPI_CALL (*debugCallback)(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType, const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData, void* pUserData) = debugCallbackDefault;
 
-    inline vector<VkQueueFamilyProperties> getQueueFamilyProperties(VkPhysicalDevice vkPhysicalDevice);
-    inline QueueFamilyIndices getQueueFamilyIndices(vector<VkQueueFamilyProperties> vkQueueFamilyProperties);
 
-    VkInstance createInstance(vector<const char *> extensions, vector<const char *> layers, const char *appName,
-                              VkDebugUtilsMessengerCreateInfoEXT vkDebugUtilsMessengerCreateInfoEXT);
-    inline void deleteInstance(VkInstance);
+    VkBool32 debugCallbackDefault(VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity, VkDebugUtilsMessageTypeFlagsEXT messageType,
+                                      const VkDebugUtilsMessengerCallbackDataEXT *pCallbackData, void *pUserData) {
+        std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
 
-    inline VkSurfaceKHR createSurface(VkInstance vkInstance, GLFWwindow *window);
-    inline void deleteSurface(VkInstance vkInstance, VkSurfaceKHR vkSurface);
+        return VK_FALSE;
+    }
 
-    vector<VkPhysicalDevice> enumeratePhysicalDevices(VkInstance vkInstance);
+    bool supportsLayers(vector<const char *> vector1);
+
+    inline VkInstance createInstance(vector<const char*> extensions, vector<const char*> layers = vector<const char*>(0),
+                                     const char* appName = "",
+                                     VkDebugUtilsMessengerCreateInfoEXT vkDebugUtilsMessengerCreateInfoEXT = vkDefaultDebugUtilsMessengerCreateInfoEXT) {
+
+        VkInstanceCreateInfo vkInstanceCreateInfo{};
+        vkInstanceCreateInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+
+        VkApplicationInfo vkAppInfo{};
+        vkInstanceCreateInfo.pApplicationInfo = &vkAppInfo;
+        vkAppInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        vkAppInfo.pApplicationName = appName;
+        vkAppInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+        vkAppInfo.pEngineName = "";
+        vkAppInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+        vkAppInfo.apiVersion = VK_API_VERSION_1_0;
+
+        vkInstanceCreateInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+        vkInstanceCreateInfo.ppEnabledExtensionNames = extensions.data();
+
+        VkDebugUtilsMessengerCreateInfoEXT createInfo{};
+        if (layers.size() != 0) {
+            if (!supportsLayers(layers)) {
+                throw std::runtime_error("requested layers not available!");
+            }
+
+            vkInstanceCreateInfo.enabledLayerCount = static_cast<uint32_t>(layers.size());
+            vkInstanceCreateInfo.ppEnabledLayerNames = layers.data();
+
+            createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+            createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+            createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+            createInfo.pfnUserCallback = debugCallback;
+
+            createInfo.pNext = (VkDebugUtilsMessengerCreateInfoEXT*) &createInfo;
+        } else {
+            vkInstanceCreateInfo.enabledLayerCount = 0;
+        }
+
+        VkInstance vkInstance;
+        vkCreateInstance(&vkInstanceCreateInfo, nullptr, &vkInstance);
+
+        return vkInstance;
+    }
+
+    inline void deleteInstance(VkInstance vkInstance) {
+        vkDestroyInstance(vkInstance, nullptr);
+    }
+
+
+    inline VkSurfaceKHR createSurface(VkInstance vkInstance, GLFWwindow* window) {
+        VkWin32SurfaceCreateInfoKHR vkCreateInfo = {};
+        vkCreateInfo.sType = VK_STRUCTURE_TYPE_WIN32_SURFACE_CREATE_INFO_KHR;
+        vkCreateInfo.hwnd = glfwGetWin32Window(window);
+        vkCreateInfo.hinstance = GetModuleHandle(nullptr);
+
+        /*if (glfwCreateWindowSurface(instance, window, nullptr, &surface) != VK_SUCCESS) { throw std::runtime_error("failed to create window surface!"); }*/
+        VkSurfaceKHR vkSurface;
+        vkCreateWin32SurfaceKHR(vkInstance, &vkCreateInfo, nullptr, &vkSurface);
+
+        return vkSurface;
+    }
+
+    inline void deleteSurface(VkInstance vkInstance, VkSurfaceKHR vkSurface) {
+        vkDestroySurfaceKHR(vkInstance, vkSurface, nullptr);
+    }
+
+    inline vector<VkPhysicalDevice> enumeratePhysicalDevices(VkInstance vkInstance) {
+        uint32_t deviceCount = 0;
+        vkEnumeratePhysicalDevices(vkInstance, &deviceCount, nullptr);
+        std::vector<VkPhysicalDevice> devices(deviceCount);
+        if (deviceCount != 0) {
+            vkEnumeratePhysicalDevices(vkInstance, &deviceCount, devices.data());
+        }
+
+        return devices;
+    }
 
     inline VkDevice createLogicalDevice(VkPhysicalDevice vkPhysicalDevice,
-                                        vector<const char*> extensions,
-                                        vector<VkDeviceQueueCreateInfo> queueCreateInfos,
-                                        VkPhysicalDeviceFeatures* features, // optional
-                                        vector <const char*> validationLayers); // optional
-    inline void deleteLogicalDevice(VkDevice device);
+                                            vector<const char*> extensions,
+                                            vector<VkDeviceQueueCreateInfo> queueCreateInfos,
+                                            VkPhysicalDeviceFeatures* features = nullptr,
+                                            vector <const char*> validationLayers = vector<const char*>(0)) {
+        VkDeviceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
 
-    bool supportsLayers(vector<const char *> layers);
+        createInfo.queueCreateInfoCount = static_cast<uint32_t>(queueCreateInfos.size());
+        createInfo.pQueueCreateInfos = queueCreateInfos.data();
+
+        createInfo.pEnabledFeatures = features;
+
+        createInfo.enabledExtensionCount = static_cast<uint32_t>(extensions.size());
+        createInfo.ppEnabledExtensionNames = extensions.data();
+
+        if (validationLayers.size() != 0) {
+            createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+            createInfo.ppEnabledLayerNames = validationLayers.data();
+        } else {
+            createInfo.enabledLayerCount = 0;
+        }
+
+        VkDevice vkDevice;
+        vkCreateDevice(vkPhysicalDevice, &createInfo, nullptr, &vkDevice);
+
+        return vkDevice;
+    }
+
+    inline void deleteLogicalDevice(VkDevice vkDevice) {
+        vkDestroyDevice(vkDevice, nullptr);
+    }
+
+    inline vector<VkQueueFamilyProperties> getQueueFamilyProperties(VkPhysicalDevice vkPhysicalDevice) {
+        uint32_t count;
+        vkGetPhysicalDeviceQueueFamilyProperties(vkPhysicalDevice, &count, nullptr);
+        vector<VkQueueFamilyProperties> queueFamilyProperties(count);
+        vkGetPhysicalDeviceQueueFamilyProperties(vkPhysicalDevice, &count, queueFamilyProperties.data());
+        return queueFamilyProperties;
+    }
+
+    // select the first queue that has present & graphics abilities
+    inline QueueFamilyIndices getQueueFamilyIndices(VkPhysicalDevice vkPhysicalDevice, VkSurfaceKHR vkSurface, vector<VkQueueFamilyProperties> vkQueueFamilyProperties) {
+        QueueFamilyIndices indices;
+
+        int i = 0;
+        for (const auto& queueFamily : vkQueueFamilyProperties) {
+            if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+                indices.graphicsFamily = i;
+            }
+
+            VkBool32 presentSupport = false;
+            vkGetPhysicalDeviceSurfaceSupportKHR(vkPhysicalDevice, i, vkSurface, &presentSupport);
+
+            if (presentSupport) {
+                indices.presentFamily = i;
+            }
+
+            if (indices.isComplete()) {
+                break;
+            }
+
+            i++;
+        }
+
+        return indices;
+    }
+
+    inline SurfaceDetails getSurfaceDetails(VkPhysicalDevice device, VkSurfaceKHR vkSurface) {
+        SurfaceDetails details;
+
+        vkGetPhysicalDeviceSurfaceCapabilitiesKHR(device, vkSurface, &details.vkSurfaceCapabilities);
+
+        uint32_t formatCount;
+        vkGetPhysicalDeviceSurfaceFormatsKHR(device, vkSurface, &formatCount, nullptr);
+
+        if (formatCount != 0) {
+            details.vkSurfaceFormats.resize(formatCount);
+            vkGetPhysicalDeviceSurfaceFormatsKHR(device, vkSurface, &formatCount, details.vkSurfaceFormats.data());
+        }
+
+        uint32_t presentModeCount;
+        vkGetPhysicalDeviceSurfacePresentModesKHR(device, vkSurface, &presentModeCount, nullptr);
+
+        if (presentModeCount != 0) {
+            details.vkPresentModes.resize(presentModeCount);
+            vkGetPhysicalDeviceSurfacePresentModesKHR(device, vkSurface, &presentModeCount, details.vkPresentModes.data());
+        }
+
+        return details;
+    }
+
+    inline bool supportsLayers(vector<const char*> layers) {
+        uint32_t layerCount;
+        vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+        std::vector<VkLayerProperties> availableLayers(layerCount);
+        vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+        for (const char* layerName : layers) {
+            bool layerFound = false;
+
+            for (const auto& layerProperties : availableLayers) {
+                if (strcmp(layerName, layerProperties.layerName) == 0) {
+                    layerFound = true;
+                    break;
+                }
+            }
+
+            if (!layerFound) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    inline VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats,
+                                                          VkFormat format = VK_FORMAT_B8G8R8A8_SRGB,
+                                                          VkColorSpaceKHR colorSpace = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
+        for (const auto& availableFormat : availableFormats) {
+            if (availableFormat.format == format && availableFormat.colorSpace == colorSpace) {
+                return availableFormat;
+            }
+        }
+
+        return availableFormats[0];
+    }
+
+    inline VkPresentModeKHR chooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes,
+                                                      VkPresentModeKHR mode = VK_PRESENT_MODE_MAILBOX_KHR) {
+        for (const auto& availablePresentMode : availablePresentModes) {
+            if (availablePresentMode == mode) {
+                return availablePresentMode;
+            }
+        }
+
+        return VK_PRESENT_MODE_FIFO_KHR;
+    }
+
+    inline VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities, uint32_t width, uint32_t height) {
+        if (capabilities.currentExtent.width != std::numeric_limits<uint32_t>::max()) {
+            return capabilities.currentExtent;
+        } else {
+            int width, height;
+
+            VkExtent2D actualExtent = {
+                    static_cast<uint32_t>(width),
+                    static_cast<uint32_t>(height)
+            };
+
+            actualExtent.width = std::clamp(actualExtent.width, capabilities.minImageExtent.width, capabilities.maxImageExtent.width);
+            actualExtent.height = std::clamp(actualExtent.height, capabilities.minImageExtent.height, capabilities.maxImageExtent.height);
+
+            return actualExtent;
+        }
+    }
 };
--- src/RenderEngine/VK/oldVK.h
+++ src/RenderEngine/VK/oldVK.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include "../glfw_vulkan.h"
 
 #define STB_IMAGE_IMPLEMENTATION
@@ -270,25 +271,6 @@ public:
 
     void populateDebugMessengerCreateInfo(
     }
-
-    void setupDebugMessenger() {
-        if (!enableValidationLayers) return;
-
-        VkDebugUtilsMessengerCreateInfoEXT createInfo;
-        populateDebugMessengerCreateInfo(createInfo);
-
-        if (CreateDebugUtilsMessengerEXT(vkInstance, &createInfo, nullptr, &debugMessenger) != VK_SUCCESS) {
-            throw std::runtime_error("failed to set up debug messenger!");
-        }
-    }
-
-    void createSurface() {
-        if (glfwCreateWindowSurface(vkInstance, window, nullptr, &vkSurface) != VK_SUCCESS) {
-            throw std::runtime_error("failed to create window surface!");
-        }
-    }
-
-
 
     void pickPhysicalDevice() {
         vector<VkPhysicalDevice> devices = enumeratePhysicalDevices();
@@ -1247,69 +1229,6 @@ public:
         }
 
         return shaderModule;
-    }
-
-    VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats) {
-        for (const auto& availableFormat : availableFormats) {
-            if (availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB && availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
-                return availableFormat;
-            }
-        }
-
-        return availableFormats[0];
-    }
-
-    VkPresentModeKHR chooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes) {
-        for (const auto& availablePresentMode : availablePresentModes) {
-            if (availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR) {
-                return availablePresentMode;
-            }
-        }
-
-        return VK_PRESENT_MODE_FIFO_KHR;
-    }
-
-    VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities) {
-        if (capabilities.currentExtent.width != std::numeric_limits<uint32_t>::max()) {
-            return capabilities.currentExtent;
-        } else {
-            int width, height;
-            glfwGetFramebufferSize(window, &width, &height);
-
-            VkExtent2D actualExtent = {
-                    static_cast<uint32_t>(width),
-                    static_cast<uint32_t>(height)
-            };
-
-            actualExtent.width = std::clamp(actualExtent.width, capabilities.minImageExtent.width, capabilities.maxImageExtent.width);
-            actualExtent.height = std::clamp(actualExtent.height, capabilities.minImageExtent.height, capabilities.maxImageExtent.height);
-
-            return actualExtent;
-        }
-    }
-
-    SwapChainSupportDetails querySwapChainSupport(VkPhysicalDevice device) {
-        SwapChainSupportDetails details;
-
-        vkGetPhysicalDeviceSurfaceCapabilitiesKHR(device, vkSurface, &details.vkSurfaceCapabilities);
-
-        uint32_t formatCount;
-        vkGetPhysicalDeviceSurfaceFormatsKHR(device, vkSurface, &formatCount, nullptr);
-
-        if (formatCount != 0) {
-            details.vkSurfaceFormats.resize(formatCount);
-            vkGetPhysicalDeviceSurfaceFormatsKHR(device, vkSurface, &formatCount, details.vkSurfaceFormats.data());
-        }
-
-        uint32_t presentModeCount;
-        vkGetPhysicalDeviceSurfacePresentModesKHR(device, vkSurface, &presentModeCount, nullptr);
-
-        if (presentModeCount != 0) {
-            details.vkPresentModes.resize(presentModeCount);
-            vkGetPhysicalDeviceSurfacePresentModesKHR(device, vkSurface, &presentModeCount, details.vkPresentModes.data());
-        }
-
-        return details;
     }
 
     bool isDeviceSuitable(VkPhysicalDevice device) {
